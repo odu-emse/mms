6630227ed76ece5cae6e4ad77404f8318e552032
Dániel B. Papp <dpapp@odu.edu>
2023-02-23 14:37:28 -0500

    feat(api): Started serving recommendation model over HTTP

--- api.py
+++ api.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from app import getUserProfile
+from recommend import Recommender
 
 app = FastAPI()
 
 
 @app.get("/recommend/")
 async def read_item(userID: str):
-    account = await getUserProfile(userID)
-    print(account)
-    return {"user": account}
+    rec = Recommender(target=userID)
+    await rec.recommend()
+    # account = await getUserProfile(userID)
+    return {"user": userID}
--- recommend.py
+++ recommend.py
@@ -3,43 +3,91 @@
 # multiply the rating by the weight of the module
 # divide the sum of the weighted ratings by the sum of the ratings
 import asyncio
+import random
 
 import pandas as pd
 
 from prisma import Prisma
 
 
-async def getRecsForUser(userID: str):
-    prism = Prisma()
-    await prism.connect()
-
-    # get a set of modules the user has reviewed
-    user_reviews = await prism.modulefeedback.find_many(where={'student': {'id': userID}})
-
-    reviews = list(map(lambda x: x.dict(), user_reviews))
-
-    # all the reviews by the user
-    df = pd.DataFrame(reviews)
-
-    all_reviews = await prism.modulefeedback.find_many()
-
-    all_reviews = list(map(lambda x: x.dict(), all_reviews))
-
-    df_all_reviews = pd.DataFrame(all_reviews)
-
-    # get the weighted average of the reviews per module
-    df['weighted_rating'] = df['rating'] * df_all_reviews['rating'].mean()
-
-    df.drop(columns=['module', 'student'], inplace=True)
-    print(df.info())
-    print(df.head())
+async def main(target: str):
+    rec = Recommender(target)
+    await rec.recommend()
 
 
+class Recommender:
+    def __init__(self, target: str):
+        self.target = target
+        self.sample = []
+        self.modules = []
+        self.recs = []
 
-async def main():
-    await getRecsForUser(userID='63f3b1cb9422322eb675292f')
-    pass
+    async def _sampleModules(self, skip: bool = True):
+        prisma = Prisma()
+        await prisma.connect()
+
+        modules = await prisma.module.find_many(include={'feedback': True})
+
+        modules = list(map(lambda x: x.dict(), modules))
+
+        rand = random.sample(modules, len(modules) // 2)
+
+        # create module feedback for sample modules for target user
+        if not skip:
+            await self._seedTargetFeedback(rand)
+
+        await prisma.disconnect()
+
+        self.sample = rand
+
+    async def _seedTargetFeedback(self, sample: list):
+        prisma = Prisma()
+        await prisma.connect()
+        for module in sample:
+            await prisma.modulefeedback.create(
+                data={
+                    'feedback': 'This is a sample review',
+                    'rating': random.randint(1, 5),
+                    'module': {
+                        'connect': {
+                            'id': module
+                        }
+                    },
+                    'student': {
+                        'connect': {
+                            'id': self.target
+                        }
+                    }
+                }
+            )
+            print(f'Created feedback for module {module}')
+
+        await prisma.disconnect()
+
+    async def recommend(self):
+        """
+        At this point our target user has reviewed half of the modules in the database.
+        We have access to the modules that the user has reviewed through the modules parameter, and now
+        we can now get recommendations for the user
+        """
+        prism = Prisma()
+        await prism.connect()
+        await self._sampleModules(skip=True)
+
+        df = pd.DataFrame(self.sample)
+
+        # convert our review data into a user x module matrix
+        # find cosine similarity between the target user and all other users
+        # get the top 5 users with the highest similarity
+        # get the modules that the top 5 users have reviewed
+        # get the modules that the target user has not reviewed
+        # get the modules that the top 5 users have reviewed that the target user has not reviewed
+        # get the average rating for each module
+        # get the top 5 modules with the highest average rating
+        # return the top 5 modules with the highest average rating as recommendations
+
+        await prism.disconnect()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(target="63f7a3068b546b91eadb20a6"))
