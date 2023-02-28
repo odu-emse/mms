2ef8743fdc73c6e92eb275249533b22008d8b66c
Dániel B. Papp <dpapp@odu.edu>
2023-02-28 12:38:36 -0500

    feat(rec): Converted movie rec system to recommender class

--- api.py
+++ api.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 
 from app import getUserProfile
-from recommend import Recommender
+from recommend import Recommender, Recs
 
 app = FastAPI()
 
 
 @app.get("/recommend/")
 async def read_item(userID: str):
-    rec = Recommender(target=userID)
-    await rec.recommend()
+    # rec = Recommender(target=userID)
+    # await rec.recommend()
     # account = await getUserProfile(userID)
-    return {"user": userID}
+
+    rec = Recs()
+    res = rec.run()
+
+    print(res.json())
+
+    return {"user": userID, "data": res}
--- recommend.py
+++ recommend.py
@@ -6,13 +6,16 @@ import asyncio
 import random
 
 import pandas as pd
+from math import sqrt
 
 from prisma import Prisma
 
 
 async def main(target: str):
-    rec = Recommender(target)
-    await rec.recommend()
+    # rec = Recommender(target)
+    # await rec.recommend()
+    rec = Recs()
+    rec.run()
 
 
 class Recommender:
@@ -87,6 +90,173 @@ class Recommender:
         # return the top 5 modules with the highest average rating as recommendations
 
         await prism.disconnect()
+
+
+class Recs:
+    def __init__(self, target=None):
+        if target is None:
+            target = [
+                {
+                    'title': 'Breakfast Club, The',
+                    'rating': 5
+                },
+                {
+                    'title': 'Toy Story',
+                    'rating': 3.5
+                },
+                {
+                    'title': 'Jumanji',
+                    'rating': 2
+                },
+                {
+                    'title': 'Pulp Fiction',
+                    'rating': 5
+                },
+                {
+                    'title': 'Akira',
+                    'rating': 4.5
+                }
+            ]
+            self.inputMovies = pd.DataFrame(target)
+        self.movies_df = pd.read_csv('input/movies.csv')
+        self.ratings_df = pd.read_csv('input/ratings.csv')
+        self.userSubsetGroup = None
+        self.pearsonCorrelationDict = dict()
+        self.tempTopUsersRating = None
+
+    def cleanData(self):
+        # Using regular expressions to find a year stored between parentheses
+        # We specify the parentheses, so we don’t conflict with movies that have years in their titles
+
+        print(self.movies_df.head())
+
+        self.movies_df['year'] = self.movies_df.title.str.extract('(\(\d\d\d\d\))', expand=False)
+
+        # Removing the parentheses
+
+        self.movies_df['year'] = self.movies_df.year.str.extract('(\d\d\d\d)', expand=False)
+
+        # Removing the years from the ‘title’ column
+
+        self.movies_df['title'] = self.movies_df.title.str.replace('(\(\d\d\d\d\))', '')
+        # Applying the strip function to get rid of any ending whitespace characters that may have appeared
+
+        self.movies_df['title'] = self.movies_df['title'].apply(lambda x: x.strip())
+
+        print(self.movies_df.head())
+
+        self.movies_df = self.movies_df.drop('genres', 1)
+
+    def handleUserInput(self):
+        inputID = self.movies_df[self.movies_df['title'].isin(self.inputMovies['title'].tolist())]
+
+        inputMovies = pd.merge(inputID, self.inputMovies)
+
+        inputMovies = inputMovies.drop('year', 1)
+
+        print(inputMovies)
+        self.inputMovies = inputMovies
+
+    def createSubset(self):
+        userSubset = self.ratings_df[self.ratings_df['movieId'].isin(self.inputMovies['movieId'].tolist())]
+
+        userSubset.head()
+
+        userSubsetGroup = userSubset.groupby(['userId'])
+
+        userSubsetGroup = sorted(userSubsetGroup, key=lambda x: len(x[1]), reverse=True)
+
+        print(userSubsetGroup[0:3])
+
+        self.userSubsetGroup = userSubsetGroup
+
+    def createSimilarityMatrix(self):
+        pearsonCorrelationDict = {}
+
+        for name, group in self.userSubsetGroup:
+            group = group.sort_values(by='movieId')
+
+            inputMovies = self.inputMovies.sort_values(by='movieId')
+
+            nRatings = len(group)
+
+            temp_df = inputMovies[inputMovies['movieId'].isin(group['movieId'].tolist())]
+            tempRatingList = temp_df['rating'].tolist()
+            tempGroupList = group['rating'].tolist()
+
+            Sxx = sum([i ** 2 for i in tempRatingList]) - pow(sum(tempRatingList), 2) / float(nRatings)
+            Syy = sum([i ** 2 for i in tempGroupList]) - pow(sum(tempGroupList), 2) / float(nRatings)
+            Sxy = sum(i * j for i, j in zip(tempRatingList, tempGroupList)) - sum(tempRatingList) * sum(
+                tempGroupList) / float(nRatings)
+
+            if Sxx != 0 and Syy != 0:
+                pearsonCorrelationDict[name] = Sxy / sqrt(Sxx * Syy)
+            else:
+                pearsonCorrelationDict[name] = 0
+
+        print(pearsonCorrelationDict.items())
+        self.pearsonCorrelationDict = pearsonCorrelationDict
+
+    def topUser(self):
+        pearsonDF = pd.DataFrame.from_dict(self.pearsonCorrelationDict, orient='index')
+        pearsonDF.columns = ['similarityIndex']
+        pearsonDF['userId'] = pearsonDF.index
+        pearsonDF.index = range(len(pearsonDF))
+
+        print(pearsonDF.head())
+
+        topUsers = pearsonDF.sort_values(by='similarityIndex', ascending=False)[0:50]
+
+        print(topUsers.head())
+
+        topUsersRating = topUsers.merge(self.ratings_df, left_on='userId', right_on='userId', how='inner')
+
+        print(topUsersRating.head())
+
+        topUsersRating['weightedRating'] = topUsersRating['similarityIndex'] * topUsersRating['rating']
+
+        print(topUsersRating.head())
+
+        tempTopUsersRating = topUsersRating.groupby('movieId').sum()[['similarityIndex', 'weightedRating']]
+
+        tempTopUsersRating.columns = ['sum_similarityIndex', 'sum_weightedRating']
+
+        print(tempTopUsersRating.head())
+
+        self.tempTopUsersRating = tempTopUsersRating
+
+    def recommend(self):
+        recommendation_df = pd.DataFrame()
+
+        recommendation_df['weighted average recommendation score'] = self.tempTopUsersRating['sum_weightedRating'] / \
+                                                                     self.tempTopUsersRating['sum_similarityIndex']
+        recommendation_df['movieId'] = self.tempTopUsersRating.index
+
+        print(recommendation_df.head())
+
+        recommendation_df = recommendation_df.sort_values(by='weighted average recommendation score', ascending=False)
+
+        print(recommendation_df.head(10))
+
+        movies_df = self.movies_df.loc[self.movies_df['movieId'].isin(recommendation_df.head(10)['movieId'].tolist())]
+
+        print(movies_df.head(10))
+
+        self.movies_df = movies_df
+
+    def convertResultToJSON(self):
+        movies = self.movies_df
+
+        return movies.to_json(orient="records")
+
+    def run(self):
+        self.cleanData()
+        self.handleUserInput()
+        self.createSubset()
+        self.createSimilarityMatrix()
+        self.topUser()
+        self.recommend()
+        return self.convertResultToJSON()
 
 
 if __name__ == '__main__':
