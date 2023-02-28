9fbc781e4848c7a188a5e9ab83205f7a26a072fa
Dániel B. Papp <dpapp@odu.edu>
2023-02-28 12:38:06 -0500

    chore(app): Cleaned up entry file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from prisma import Prisma
 from utils.db import testConnection
 from utils.seed import Seeder, Skipper
 
+
 async def main() -> None:
     print('Starting application...')
     testConnection()
@@ -17,28 +18,8 @@ async def main() -> None:
     await db.seedAll()
     await db.cleanupAll()
     # targetID = await db.createTargetUser()
-    await getUserProfile(ID='63f7a3068b546b91eadb20a6')
 
     exit(0)
-
-
-async def getUserProfile(ID):
-    print('Fetching user data...')
-    # get user from ID
-    #  find enrolled modules
-    #  remove modules that already enrolled in
-    #  get feedback for each module
-    #  get similarity matrix
-    #  get recommendations
-    #  return recommendations
-    prisma = Prisma()
-    await prisma.connect()
-    account = await prisma.user.find_unique(
-        where={
-            'id': ID
-        }
-    )
-    print(account)
 
 
 if __name__ == '__main__':
