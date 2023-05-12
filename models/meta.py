896bd885c67cb5ccc8e2b6b083bc1510895c2cb4
Dániel B. Papp <dpapp@odu.edu>
2023-05-12 12:22:35 -0400

    feat(api): configured model to be served over HTTP

--- models/meta.py
+++ models/meta.py
@@ -6,7 +6,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 class MetaRecommender:
-    def __init__(self, path: str, target: str):
+    """
+    This class is used to recommend modules based on the module name, description, keywords and objectives. This is commonly
+    referred to as a Meta-Based Recommender System. The class takes in a path to a json file, a target module name and a
+    size of the recommendation list. The class will then return a list of
+    recommended modules based on the target module name.
+    """
+
+    def __init__(self, path: str, target: str, size: int = 10):
         self.df = pd.read_json(path)
         self.featureDf = pd.DataFrame()
         self.target = target
@@ -14,15 +21,23 @@ class MetaRecommender:
         self.stop = stopwords.words("english")
         self.cv = CountVectorizer()
         self.similarity = None
+        self.size = size
 
-    def _convert_module_id(self, df: pd.DataFrame()):
+    @staticmethod
+    def _convert_module_id(df: pd.DataFrame()):
+        """
+        This function is used to convert the module id from a mongo Object ID to a string.
+        :param df: the dataframe to be modified by the apply function
+        :return: the modified dataframe
+        """
         temp_id = df["id"]
         temp_id = dict(temp_id)
         temp_id = temp_id["$oid"]
         df["_id"] = temp_id
         return df
 
-    def _scalar_to_str(self, df: pd.DataFrame()):
+    @staticmethod
+    def _scalar_to_str(df: pd.DataFrame()):
         lst_k = list(df["keywords"])
         lst_o = list(df["objectives"])
 
@@ -81,29 +96,49 @@ class MetaRecommender:
         self.similarity = cosine_similarity(count_matrix)
 
     def _get_recommendations(self):
-        module_id = self.df[self.df["moduleName"] == self.target]["id"].values[0]
+        try:
+            module_id = self.df[
+                self.df["moduleName"].str.lower() == str(self.target).lower()
+            ]["id"].values[0]
+            score = list(enumerate(self.similarity[module_id]))
 
-        score = list(enumerate(self.similarity[module_id]))
+            sorted_score = sorted(score, key=lambda x: x[1], reverse=True)
 
-        sorted_score = sorted(score, key=lambda x: x[1], reverse=True)
+            sorted_score = sorted_score[1:]
 
-        sorted_score = sorted_score[1:]
+            if self.size > len(sorted_score):
+                self.size = len(sorted_score)
 
-        results = []
+            results = []
 
-        i = 0
-        for item in sorted_score:
-            module_title = self.df[self.df["id"] == item[0]]["moduleName"].values[0]
-            module_oid = self.df[self.df["id"] == item[0]]["_id"].values[0]
-            print(i + 1, module_title, module_oid, item[1])
-            results.append({"title": module_title, "id": module_oid, "score": item[1]})
-            i += 1
-            if i > 10:
-                break
+            i = 0
+            for item in sorted_score:
+                module_title = self.df[self.df["id"] == item[0]]["moduleName"].values[0]
+                module_oid = self.df[self.df["id"] == item[0]]["_id"].values[0]
+                print(i + 1, module_title, module_oid, item[1])
+                results.append(
+                    {"title": module_title, "id": module_oid, "score": item[1]}
+                )
+                i += 1
+                if i > self.size - 1:
+                    break
 
-        return results
+            return results
+        except IndexError:
+            return {"error": "Module not found"}
 
     def run(self):
+        """
+        This function is used to run the recommender system. It recommends modules based on the
+        target module name passed in. The list will be sorted by the cosine similarity score.
+        The list will be in the format of
+
+        *[{"title": module_title, "id": module_oid, "score": cosine_similarity_score}, ...]*
+
+        .. todo:: Add a check to see if the target module name is in the database.
+
+        .. return:: a list of recommended modules or a dictionary an error message
+        """
         self._clean_data(self._convert_module_id, self._scalar_to_str)
         self._create_features()
         self.featureDf = self.df[["id", "features"]]
@@ -111,10 +146,6 @@ class MetaRecommender:
             self.featureDf["features"]
         )
 
-        print(self.featureDf.head(10))
-
         self._get_cosine()
-
-        print(self.similarity)
 
         return self._get_recommendations()
